c900be2bd2b1961016a114ce7fe9964b2d78bedb
nick.lelarge <nick.lelarge@ka-raceing.de>
2020-10-18 14:51:50 +0200

    Added enable pins.

--- motorModule.py
+++ motorModule.py
@@ -2,18 +2,20 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 class Motor():
-    def __init__(self, In1, In2):
+    def __init__(self, In1, In2, En):
         self.In1 = In1
         self.In2 = In2
+        self.En = En
         GPIO.setup(self.In1,GPIO.OUT)
         GPIO.setup(self.In2,GPIO.OUT)
-        self.pwmA = GPIO.PWM(self.In1, 100)
-        self.pwmB = GPIO.PWM(self.In2, 100)
-        self.pwmA.start(0)
-        self.pwmB.start(0)
+        GPIO.setup(self.En,GPIO.OUT)
+        self.pwm = GPIO.PWM(self.En, 100)
+        self.pwm.start(0)
 
     def move(self, speed=0.5):
+        # check requested direction
         forward = speed >= 0
+        
         # scale normalized speed to value between -100 and 100
         speed *= 100
         speed = abs(speed)
@@ -21,14 +23,15 @@ class Motor():
 
         # forward
         if forward:
-            self.pwmA.ChangeDutyCycle(speed)
+            GPIO.output(self.In1, GPIO.High)
             GPIO.output(self.In2, GPIO.LOW)
         # backward
         else:
-            self.pwmB.ChangeDutyCycle(speed)
             GPIO.output(self.In1, GPIO.LOW)
+            GPIO.output(self.In2, GPIO.High)
+        
+        self.pwm.ChangeDutyCycle(speed)
 
     def stop(self):
         # stop motor
-        self.pwmA.ChangeDutyCycle(0)
-        self.pwmB.ChangeDutyCycle(0)
+        self.pwm.ChangeDutyCycle(0)
